dac11ec0eca31f21e2b5e687f54b05205ed7311a
Arindam Dey <biplabhome@gmail.com>
2020-05-14 23:02:33 +0530

    Blank Test Project Added

---
+++ NumberToWordTests/NumericLibraryTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumberToWord;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberToWord.Tests
+{
+    [TestClass()]
+    public class NumericLibraryTests
+    {
+        [TestMethod()]
+        public void NumberToWordTest()
+        {
+            Assert.Fail();
+        }
+    }
+}

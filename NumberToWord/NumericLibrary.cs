92a4aeea253995945470d00b67afad81ac6d7b5c
Arindam Dey <biplabhome@gmail.com>
2020-05-14 22:59:17 +0530

    Blank Library Added To approach for TDD

---
+++ NumberToWord/NumericLibrary.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace NumberToWord
+{
+    public class NumericLibrary
+    {
+        
+        public string NumberToWord(int num)
+        {
+            int n = num;
+            StringBuilder result = new StringBuilder();
+            return result.ToString();
+        }
+    }
+}

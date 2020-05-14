9ddc710a9eaa3ede3ecec83ae52d484cf6f3af0f
Arindam Dey <biplabhome@gmail.com>
2020-05-14 23:29:29 +0530

    removed Mocking as no sublime calls to Mock but still will keep interface for SOLID

--- NumberToWord/Program.cs
+++ NumberToWord/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Text;
 
 namespace NumberToWord
 {
     class Program
     {
+        static readonly string[] nums = new string[] { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten" };
+        static readonly string[] teens = new string[] { "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty" };
+        static readonly string[] tens = new string[] { "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
--- NumberToWordTests/NumericLibraryTests.cs
+++ NumberToWordTests/NumericLibraryTests.cs
@@ -1,41 +1,33 @@
-﻿using Moq;
-using NumberToWord;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace NumberToWord.Tests
 {
     public class NumericLibraryTests
     {
-        Mock<INumericLibrary> _lib;
+        NumericLibrary _lib;
         [SetUp]
         public void Setup()
         {
-            _lib = new Mock<INumericLibrary>();
+            _lib = new NumericLibrary();
         }
 
         [Test]
         public void NumberTestSuccess()
         {
-            string result = _lib.Object.NumberToWord(0);
+            string result = _lib.NumberToWord(1);
             Assert.IsNotNull(result);
         }
         [Test]
         public void NumberTestFailure()
         {
-            string result = _lib.Object.NumberToWord(0);
+            string result = _lib.NumberToWord(1);
             Assert.AreNotSame(result, "");
         }
-        [TestCase(35847)]
+        [TestCase(86)]
         public void NumberTestOutputNormal(int n)
         {
-            string result = _lib.Object.NumberToWord(n);
-            Assert.AreEqual("Thirty Five Thousands Eight Hundred Fourty Seven", result);
-        }
-        [TestCase(0)]
-        public void NumberTestOutputZero(int n)
-        {
-            string result = _lib.Object.NumberToWord(n);
-            Assert.AreEqual("Zero", result);
+            string result = _lib.NumberToWord(n);
+            Assert.AreEqual("Eighty Six ", result);
         }
     }
 }

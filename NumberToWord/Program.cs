e5b96995c69a14a26b705d6560e6a9bc36187a5a
Arindam Dey <biplabhome@gmail.com>
2020-05-14 23:34:27 +0530

    moved monolithic to seperate file for SOLID

--- NumberToWord/Program.cs
+++ NumberToWord/Program.cs
@@ -5,44 +5,13 @@ namespace NumberToWord
 {
     class Program
     {
-        static readonly string[] nums = new string[] { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten" };
-        static readonly string[] teens = new string[] { "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty" };
-        static readonly string[] tens = new string[] { "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
-
         static void Main(string[] args)
         {
-            var num = Console.ReadLine();
-            int n = Int32.Parse(num);
-            StringBuilder result = new StringBuilder();
-            
-            int count = 0;
-            while (true)
+            var w = new NumericLibrary();
+            for (var i = 1; i < 100; i++)
             {
-                int r = n % 10;
-                n /= 10;
-                result.Insert(0, " ");
-
-                if (count == 1) //Teens
-                {
-                    result.Insert(0, tens[r]);
-                }
-                else
-                {
-                    result.Insert(0, nums[r]);
-                    if (count == 2)//hundred
-                        result.Insert(0, " Hundred");
-                    else if (count == 3)
-                        result.Insert(0, " Thousand");
-                }
-                if (n < 10)
-                {
-                    result.Insert(0, " ");
-                    result.Insert(0, tens[n - 2]);
-                    break;
-                }
-                count++;
+                Console.WriteLine("Number: {0} InWords: {1}", i, w.NumberToWord(i));
             }
-            Console.WriteLine(result.ToString());
         }
     }
 }

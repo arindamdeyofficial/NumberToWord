48d3ef3dad4442dbbe7aee484dcb466cc1dcdabf
Arindam Dey <biplabhome@gmail.com>
2020-05-14 23:32:05 +0530

    programming monolithic first...just looping through the number to unvail number system

--- NumberToWord/Program.cs
+++ NumberToWord/Program.cs
@@ -11,7 +11,38 @@ namespace NumberToWord
 
         static void Main(string[] args)
         {
+            var num = Console.ReadLine();
+            int n = Int32.Parse(num);
+            StringBuilder result = new StringBuilder();
             
+            int count = 0;
+            while (true)
+            {
+                int r = n % 10;
+                n /= 10;
+                result.Insert(0, " ");
+
+                if (count == 1) //Teens
+                {
+                    result.Insert(0, tens[r]);
+                }
+                else
+                {
+                    result.Insert(0, nums[r]);
+                    if (count == 2)//hundred
+                        result.Insert(0, " Hundred");
+                    else if (count == 3)
+                        result.Insert(0, " Thousand");
+                }
+                if (n < 10)
+                {
+                    result.Insert(0, " ");
+                    result.Insert(0, tens[n - 2]);
+                    break;
+                }
+                count++;
+            }
+            Console.WriteLine(result.ToString());
         }
     }
 }
